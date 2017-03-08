549e89bfde69c2dc932ae700e62d9fe2e5b0b0c6
Philip Bazzard <pbazzard@gmail.com>
2017-03-08 22:44:19 +0000

    working version of counting letter

--- countLetters.js
+++ countLetters.js
@@ -1,39 +1,30 @@
 var args = process.argv.slice(2);
 
 
-
 function countLetters(stringInput){
   stringInput = stringInput.join("");
   //split deletes all whitespace from stringInput
   stringInput = stringInput.split("");
-  // gets all letters from A-Z
-  var count = 0;
+
   var object = {};
   for (var i = 0; i < stringInput.length; i++){
     var character = stringInput[i];
-
-      if (character[i] = stringInput){
-      count = count + 1;
+    //console.log(object[character]);
+    //this line checks if character comes for the first time
+    if (object[character] === undefined){
+      object[character] = 1;
+      //console.log(object[character]);
+    }
+    else{
+      object[character] = object[character] + 1;
+      //console.log(object[character]);
     }
 
-    object[character] = count;
   //console.log(character);
   }
   console.log(object);
 };
 
-
-
-
 countLetters(args);
 
 
-//take all the letters and break them up
-//add them to an associative array
-//count letters if unique
-//print them
-
-
-//count letters that return unique
-
-//count add if found
